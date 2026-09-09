75e9f1d9706c2283da3a137ca51a7fa857b59430
RAMJAN <niloyahmadramjan@gmail.com>
2026-09-09 11:26:17 +0800

    created function for published delete get schedule

--- src/app/module/schedule/schedule.service.ts
+++ src/app/module/schedule/schedule.service.ts
@@ -1,11 +1,21 @@
-import { addDays, differenceInMinutes, startOfDay } from "date-fns";
+import {
+  addDays,
+  differenceInMinutes,
+  isAfter,
+  isSameDay,
+  startOfDay,
+} from "date-fns";
 import { prisma } from "../../lib/prisma";
 import { RequestUser } from "../../middleware/checkAuth";
 import { AppError } from "../../utils/AppError";
-import { ICreateSchedulePayload } from "./schedule.interface";
+import {
+  ICreateSchedulePayload,
+  IUpdateSchedulePayload,
+} from "./schedule.interface";
 import httpStatus from "http-status";
 import { IQuery } from "../doctor/doctor.interface";
 import { ScheduleWhereInput } from "../../../generated/prisma/models";
+import { ScheduleStatus } from "../../../generated/prisma/enums";
 
 const createShedule = async (
   payload: ICreateSchedulePayload,
@@ -229,9 +239,284 @@ const getScheduleByid = async (scheduleid: string) => {
   return schedule;
 };
 
+const updateSchedule = async (
+  scheduleId: string,
+  payload: IUpdateSchedulePayload,
+  user: RequestUser,
+) => {
+  const doctor = await prisma.doctor.findUnique({
+    where: { userId: user.userId },
+  });
+
+  if (!doctor) {
+    throw new AppError(httpStatus.NOT_FOUND, "Doctor profile not found");
+  }
+
+  const schedule = await prisma.schedule.findUnique({
+    where: { id: scheduleId, doctorId: doctor.id },
+  });
+
+  if (!schedule || schedule.isDeleted) {
+    throw new AppError(httpStatus.NOT_FOUND, "Schedule Not Found");
+  }
+  if (
+    schedule.status === ScheduleStatus.PUBLISHED &&
+    schedule.totalSlots !== schedule.availableSlots
+  ) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "Schedule one published and appointment booked cannot be updated",
+    );
+  }
+
+  if (schedule.doctorId !== doctor.id) {
+    throw new AppError(
+      httpStatus.FORBIDDEN,
+      "You are not allowed to update this schedule",
+    );
+  }
+
+  payload.meetingLink = payload.meetingLink || schedule.meetingLink;
+  payload.startDateTime = payload.startDateTime || schedule.startDateTime;
+  payload.endDateTime = payload.endDateTime || schedule.endDateTime;
+
+  // 25 August => start Time  : 9:00 PM
+  // 26 August => end Time : 3:00AM
+
+  if (!isSameDay(payload.startDateTime, payload.endDateTime)) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "Start date time and end date time must be on the same day",
+    );
+  }
+
+  if (isAfter(payload.startDateTime, payload.endDateTime)) {
+    // 25 August =>  3:00 PM - 9:00 PM
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "Start Date time cannot be after end data time",
+    );
+  }
+
+  //startDateTime = 2026-08-25T13:30:00.436Z => 1:30 PM
+
+  const startOfTheDay = startOfDay(payload.startDateTime); // 25 August => 12:00 AM => 2026-08-25T00:00:00.436Z
+  const startOfNextDay = addDays(startOfTheDay, 1); // 26 August => 12:00 AM => 2026-08-26T00:00:00.436Z
+
+  const existingScheduleOnThisDate = await prisma.schedule.findFirst({
+    where: {
+      doctorId: doctor.id,
+      isDeleted: false,
+      startDateTime: {
+        gte: startOfTheDay,
+        lt: startOfNextDay,
+      },
+    },
+  });
+
+  if (existingScheduleOnThisDate) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "You Already Have A Schedule For This Date",
+    );
+  }
+
+  const durationMinute = differenceInMinutes(
+    payload.endDateTime,
+    payload.startDateTime,
+  );
+
+  const MINUTES_ALLOCATED_PER_SLOT = 20;
+  const totalSlots = Math.floor(durationMinute / MINUTES_ALLOCATED_PER_SLOT);
+  if (totalSlots < 1) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      `Schedule Must Be At Least ${MINUTES_ALLOCATED_PER_SLOT} Minutes Long To Fit One Slot`,
+    );
+  }
+
+  const updateSchedule = await prisma.schedule.update({
+    where: { id: schedule.id },
+    data: {
+      startDateTime: payload.startDateTime,
+      endDateTime: payload.endDateTime,
+      meetingLink: payload.meetingLink,
+      totalSlots,
+      availableSlots: totalSlots,
+      doctorId: doctor.id,
+    },
+    include: {
+      doctor: {
+        select: {
+          name: true,
+          email: true,
+          contactNumber: true,
+        },
+      },
+    },
+  });
+
+  return updateSchedule;
+};
+
+const publishedShedule = async (scheduleId: string, user: RequestUser) => {
+  const doctor = await prisma.doctor.findUnique({
+    where: {
+      userId: user.userId,
+    },
+  });
+
+  if (!doctor) {
+    throw new AppError(httpStatus.NOT_FOUND, "Doctor profile not found");
+  }
+  const schedule = await prisma.schedule.findUnique({
+    where: {
+      id: scheduleId,
+      doctorId: doctor.id,
+    },
+  });
+
+  if (!schedule || schedule.isDeleted) {
+    throw new AppError(httpStatus.NOT_FOUND, "Schedule is not found");
+  }
+
+  if (schedule.status === ScheduleStatus.PUBLISHED) {
+    throw new AppError(httpStatus.CONFLICT, "Schedule is already published");
+  }
+  const publishedSchedule = await prisma.schedule.update({
+    where: {
+      id: schedule.id,
+    },
+    data: {
+      status: ScheduleStatus.PUBLISHED,
+    },
+  });
+
+  return publishedSchedule;
+};
+
+const deleteSchedule = async (scheduleId: string, user: RequestUser) => {
+  const doctor = await prisma.doctor.findUnique({
+    where: { userId: user.userId },
+  });
+
+  if (!doctor) {
+    throw new AppError(httpStatus.NOT_FOUND, "Doctor profile not found");
+  }
+
+  const schedule = await prisma.schedule.findUnique({
+    where: { id: scheduleId, doctorId: doctor.id },
+  });
+
+  if (!schedule || schedule.isDeleted) {
+    throw new AppError(httpStatus.NOT_FOUND, "Schedule not found");
+  }
+
+  if (
+    schedule.status === ScheduleStatus.PUBLISHED &&
+    schedule.totalSlots !== schedule.availableSlots
+  ) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "Schedule onece published and appointment booked cannot be deleted",
+    );
+  }
+
+  const deletedSchedule = await prisma.schedule.update({
+    where: { id: schedule.id },
+    data: {
+      isDeleted: true,
+      deletedAt: new Date(),
+    },
+  });
+
+  return deletedSchedule;
+};
+
+const getTodaysSchedules = async (query: IQuery) => {
+  if (!query.doctorId) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      "Doctor id must be provided in query",
+    );
+  }
+
+  const doctor = await prisma.doctor.findUnique({
+    where: { id: query.doctorId },
+  });
+  if (!doctor) {
+    throw new AppError(httpStatus.NOT_FOUND, "Doctor profile not found");
+  }
+
+  const limit = query.limit ? Number(query.limit) : 10;
+  const page = query.page ? Number(query.page) : 1;
+  const skip = (page - 1) * limit;
+  const sortBy = query.sortBy ? query.sortBy : "createdAt";
+  const sortOrder = query.sortOrder ? query.sortOrder : "desc";
+
+  const now = new Date();
+  const startOfToday = startOfDay(now);
+  const startOfTomorrow = addDays(startOfToday, 1);
+
+  const andConditions: ScheduleWhereInput[] = [
+    {
+      doctorId: query.doctorId,
+    },
+    {
+      isDeleted: false,
+    },
+    {
+      status: ScheduleStatus.PUBLISHED,
+    },
+    {
+      startDateTime: {
+        gte: startOfToday,
+        lt: startOfTomorrow,
+        gt: now,
+      },
+    },
+    {
+      availableSlots: {
+        gt: 0,
+      },
+    },
+  ];
+
+  const schedule = await prisma.schedule.findMany({
+    where: {
+      AND: andConditions,
+    },
+    take: limit,
+    skip,
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
+  });
+
+  const total = await prisma.schedule.count({
+    where: {
+      AND: andConditions,
+    },
+  });
+
+  return {
+    data: schedule,
+    meta: {
+      page,
+      limit,
+      total,
+      totalPages: Math.ceil(total / limit),
+    },
+  };
+};
+
 export const ScheduleService = {
   createShedule,
   getMySchedule,
   getAllSchedules,
   getScheduleByid,
+  updateSchedule,
+  publishedShedule,
+  deleteSchedule,
+  getTodaysSchedules,
 };
